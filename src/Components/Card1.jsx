24fb0044e9b154e35fdd5a8b05c1f452c27f4a4c
ipsita <ipsita.panda.2001@gmail.com>
2021-07-07 18:58:32 +0530

    changing the style and position of card1

--- src/Components/Card1.jsx
+++ src/Components/Card1.jsx
@@ -2,9 +2,9 @@ import Card from "react-bootstrap/Card";
 export const Card1 = () => {
   return (
     <div>
-      <Card>
-        <Card.Body className="cardBody">
-          <Card.Title>Emoji Pedia </Card.Title>
+      <Card style={{ width: "25rem" }} className="cardAllignment">
+        <Card.Body>
+          <Card.Title className="cardBody">Emoji Pedia </Card.Title>
         </Card.Body>
       </Card>
     </div>
