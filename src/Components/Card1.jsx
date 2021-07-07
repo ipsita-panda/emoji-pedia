04bc5fc5b6fa583255ab02071bdd06e8265fed1f
ipsita <ipsita.panda.2001@gmail.com>
2021-07-07 18:38:25 +0530

    initial commit

---
+++ src/Components/Card1.jsx
@@ -0,0 +1,12 @@
+import Card from "react-bootstrap/Card";
+export const Card1 = () => {
+  return (
+    <div>
+      <Card>
+        <Card.Body className="cardBody">
+          <Card.Title>Emoji Pedia </Card.Title>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
